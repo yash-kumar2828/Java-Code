e4a11da4d1d9f9658f5913309b9149c11b0d0cd6
yash kumar <you@example.com>
2025-08-14 19:08:38 +0530

    variable type

---
+++ Variables.java
@@ -0,0 +1,28 @@
+public class Variables{
+    public static void main(String[] args) {
+        int num=74;
+        System.out.println(num);
+
+
+        float num2=74.0f;
+        System.out.println(num2);
+
+
+        double num3=74.8989;
+        System.out.println(num3);
+
+
+
+        boolean isVegetarian=false;
+        System.out.println(isVegetarian);
+
+
+
+        String name="yash kumar";
+        System.out.println(name);
+
+
+        char word='Y';
+        System.out.println(word);
+    }
+}
