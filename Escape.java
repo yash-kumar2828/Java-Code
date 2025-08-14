81eeed0a37bfb436208e4739e85f9e74dee82b42
yash kumar <you@example.com>
2025-08-14 19:09:17 +0530

    use of escape

---
+++ Escape.java
@@ -0,0 +1,10 @@
+public class Escape{
+    public static void main(String[] args) {
+        System.out.println("Hello 'Yash Kumar'");
+        System.out.println("Hello \"Yash Kumar\" ");
+        System.out.println("Hello \\Yash Kumar\\");
+        System.out.println("Hello \nYash Kumar");
+        System.out.println("Hello \tYash Kumar");
+        System.out.println("Hello \bYash Kumar");
+    }
+}
