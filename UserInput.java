a9427dd139f9180c46c0538037af02ce6ff0f0f2
yash kumar <you@example.com>
2025-08-14 19:09:51 +0530

    use of user input

---
+++ UserInput.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class UserInput{
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter Your Name=");
+        String name =input.nextLine();
+        System.out.println("Good Morning "+name);
+        System.out.print(name+", Also tell me your age:");
+        int age=input.nextInt();
+        System.err.println("your age is "+age);
+    }
+}
