d2932e9a80c45e6f0c8650e77b09d8a217555770
yash kumar <you@example.com>
2025-08-15 14:23:42 +0530

    order of operation

---
+++ OrderOfOperation.java
@@ -0,0 +1,9 @@
+public class OrderOfOperation{
+    public static void main(String[] args) {
+        System.out.println(8-3*3);
+        System.out.println(9/3/3);
+        System.out.println(9/(3/3+2));
+        System.out.println(9/3*2/6);
+        System.out.println(8-5+7-1);
+    }
+}
