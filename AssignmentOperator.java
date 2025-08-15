f05c3ead2975ea5c2e62ee9bd3ccd26cacdf588a
yash kumar <you@example.com>
2025-08-15 07:17:17 +0530

    Assignment Operator

---
+++ AssignmentOperator.java
@@ -0,0 +1,8 @@
+public class AssignmentOperator{
+    public static void main(String[] args) {
+        int num=9;
+        System.err.println(num);
+        int num2=num;
+        System.err.println(num2);
+    }
+}
