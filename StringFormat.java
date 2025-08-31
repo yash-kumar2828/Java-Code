31980689d3df9f82d9d886331bbc49e2f482be1f
yash kumar <you@example.com>
2025-08-31 15:09:51 +0530

    StringFormat

---
+++ StringFormat.java
@@ -0,0 +1,7 @@
+public class StringFormat{
+    public static void main(String[] args) {
+        int marks=95;
+        String name="yash kumar";
+        System.out.printf("Hello %10S, your marks are: %5d",name,marks);
+    }
+}
