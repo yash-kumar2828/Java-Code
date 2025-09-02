85285b1ede6571738c42177178e78ec9bfd79589
yash kumar <you@example.com>
2025-09-02 15:16:25 +0530

    TwoWheeler

---
+++ Encapsulation and Inheritance/TwoWheeler.java
@@ -0,0 +1,9 @@
+public class TwoWheeler extends Vehicle{
+
+    public TwoWheeler() {
+        setNumberOfTires(2);
+    }
+    public void balance(){
+        System.out.println("I am balancing on two tires");
+    }
+}
