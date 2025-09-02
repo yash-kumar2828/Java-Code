57ea4044f5a31a1f9912af8504ee529b98e4b1ae
yash kumar <you@example.com>
2025-09-02 15:15:59 +0530

    Vehicle

---
+++ Encapsulation and Inheritance/Vehicle.java
@@ -0,0 +1,13 @@
+public class Vehicle{
+    protected   int numberOfTires;
+    public void setNumberOfTires(int numberOfTires){
+        this.numberOfTires=numberOfTires;
+    }
+    @Override
+    public String toString(){
+        return "Vehicle no of tires "+numberOfTires;
+    }
+    public void commute(){
+        System.out.printf("I am going from place A to place B using %d tires\n",numberOfTires);
+    }
+}
